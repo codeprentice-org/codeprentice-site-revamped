f24b208142b96013c62446a815b4a28243bd74fa
Devin Leamy <devinleamy@gmail.com>
2020-11-06 20:48:50 -0500

    create project controller

--- backend/controllers/project.controller.ts
+++ backend/controllers/project.controller.ts
@@ -1,1 +1,28 @@
-// Placeholder text
+import { Request, Response, NextFunction } from "express";
+import { ProjectModel } from "../models/project.model";
+import { UserModel } from "../models/user.model";
+import { Types, Schema } from "mongoose";
+
+// Creates a new project give a request body containing project: { name: string }
+// Just for testing
+const createProject = async (req: Request, res: Response, next: NextFunction) => {
+    const projectInfo = req.body.project;
+    const teamMember = await UserModel.findOne({ _id: req.body.user._id }).exec(); 
+    const newProject = new ProjectModel({
+        _id: new Types.ObjectId(),
+        name: projectInfo.name,
+        team: [(teamMember)? teamMember.toObject(): undefined]
+    });
+    newProject.save()
+              .then((resolve) => {
+                  console.log(resolve);
+                  res.status(200)
+                    .send("Project was successfully created");
+              })
+              .catch(() => {
+                  res.status(401)
+                     .send("Unable to create new project");
+              });
+};
+
+export { createProject }
--- backend/routes/project.ts
+++ backend/routes/project.ts
@@ -1,32 +1,9 @@
 import express from "express";
-import { Request, Response, NextFunction } from "express";
-import { ProjectModel } from "../models/project.model";
-import { UserModel } from "../models/user.model";
-import { checkAuth }  from "../middleware/check-auth";
+import { checkAuth } from "../middleware/check-auth";
 import { checkRoleAdmin } from "../middleware/check-role";
-import { Types, Schema } from "mongoose";
+import { createProject } from "../controllers/project.controller";
 
 export const PROJECT_API = express.Router();
 
-
 // Creates a new project given a request body containing project: { name: string }
-// Just for testing
-PROJECT_API.post("/create_project", checkAuth, checkRoleAdmin, async (req: Request, res: Response, next: NextFunction) => {
-    const projectInfo = req.body.project;
-    const teamMember = await UserModel.findOne({ _id: req.body.user._id }).exec(); 
-    const newProject = new ProjectModel({
-        _id: new Types.ObjectId(),
-        name: projectInfo.name,
-        team: [(teamMember)? teamMember.toObject(): undefined]
-    });
-    newProject.save()
-              .then((resolve) => {
-                  console.log(resolve);
-                  res.status(200)
-                    .send("Project was successfully created");
-              })
-              .catch(() => {
-                  res.status(401)
-                     .send("Unable to create new project");
-              });
-});
+PROJECT_API.post("/create_project", checkAuth, checkRoleAdmin, createProject); 
