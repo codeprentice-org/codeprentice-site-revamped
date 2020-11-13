fd6d4bade1d1b916372efc00c5eea03e6bd2e117
Noredeen Al-Zu'bi <noredeenalzubi@gmail.com>
2020-11-13 18:47:54 +0200

    Add testing boilerplate.

--- backend/controllers/user.controller.ts
+++ backend/controllers/user.controller.ts
@@ -6,39 +6,69 @@ import jwt from "jsonwebtoken";
 import { Secret } from "jsonwebtoken";
 import { ROLE } from "../enums/role";
 import dotenv from "dotenv";
+import { AxiosResponse } from "axios";
+import qs from "qs";
+import axios from "axios";
 
 dotenv.config({ path: "./backend/config/.env.config" });
 
 const loginUser = async (req: Request, res: Response, next: NextFunction) => {
-    // authentication with github api
-    // adds user to database if they are not there already
-    // pull user from database
-     // temporary static user 
-    const userData = req.body.user;
-    await UserModel.findOne({email: userData.email})
-        .exec()
-        .then(async (resolve) =>  {
-            if (!resolve) {
-                return res.status(401).json({
-                    status: 1,
-                    data: "Authentication Failed"
-                });
-            }
-            const user: UserType = resolve.toObject();
-            const token = jwt.sign(user, process.env.JWT_KEY as Secret, { expiresIn: '1h' });
-            console.log(`User with email: ${user.email} logged in`);
-            return res.status(200).json({
-                status: 0,
-                data: { token: token }
-            }); 
-        })
-        .catch(() => {
-            return res.status(401).json({
-                status: 1,
-                data: "Authentication Failed"
-            });
-        })
+    // Redirect caller to the GitHub auth web flow with our app credentials
+    res.redirect("https://github.com/login/oauth/authorize?" + qs.stringify({
+        client_id: process.env.GITHUB_CLIENT_ID,
+        scope: "read:org",
+        redirect_uri: "http://localhost:4200/user/callback"
+    }))
 };
+
+const gitHubCallback = async (req: Request, res: Response, next: NextFunction) => {
+    // After successfull auth on GitHub, a code is sent back. We POST it back
+    // to exchange it for an access token
+    const body = {
+        client_id: process.env.GITHUB_CLIENT_ID,
+        client_secret: process.env.GITHUB_CLIENT_SECRET,
+        code: req.query.code // The code we received from GitHub
+    };
+    const opts = { headers: { accept: 'application/json' } };
+    axios.post(`https://github.com/login/oauth/access_token`, body, opts)
+    .then((post_res:AxiosResponse) => {
+        axios.get('https://api.github.com/user/memberships/orgs/codeprentice-org',
+        { headers: { authorization: "token" + post_res.data.access_token, accept: "application/vnd.github.v3+json" } })
+        .then((r:AxiosResponse) => qs.parse(r.data));
+    })
+    .catch((err:Error) => res.status(500).json({ message: err.message }));
+};
+
+// const loginUser = async (req: Request, res: Response, next: NextFunction) => {
+//     // authentication with github api
+//     // adds user to database if they are not there already
+//     // pull user from database
+//      // temporary static user 
+//     const userData = req.body.user;
+//     await UserModel.findOne({email: userData.email})
+//         .exec()
+//         .then(async (resolve) =>  {
+//             if (!resolve) {
+//                 return res.status(401).json({
+//                     status: 1,
+//                     data: "Authentication Failed"
+//                 });
+//             }
+//             const user: UserType = resolve.toObject();
+//             const token = jwt.sign(user, process.env.JWT_KEY as Secret, { expiresIn: '1h' });
+//             console.log(`User with email: ${user.email} logged in`);
+//             return res.status(200).json({
+//                 status: 0,
+//                 data: { token: token }
+//             }); 
+//         })
+//         .catch(() => {
+//             return res.status(401).json({
+//                 status: 1,
+//                 data: "Authentication Failed"
+//             });
+//         })
+// };
 
 // Changes user username give a request body container user: { newUsername: string }
 // Just for testing
@@ -86,4 +116,4 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
 };
 
 
-export { loginUser, changeUsername, createUser }
+export { loginUser, changeUsername, createUser, gitHubCallback }
