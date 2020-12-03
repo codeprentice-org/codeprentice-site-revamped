6cbc87bb338f858d1f97bac69ec89448a4f1a306
Ansh Dhingra <56676059+anshdhinhgra47@users.noreply.github.com>
2020-12-03 11:08:44 +0530

    Updated main.tsx

--- frontend/src/components/main/main.tsx
+++ frontend/src/components/main/main.tsx
@@ -24,7 +24,7 @@ const Main: React.FC<MainProps> = () => {
                 <Switch>
                     <Route path="/about" exact component={Home}></Route>
                     <Route path="/projects" exact component={Projects}></Route>
-                     <Route path="/projects/:name" exact component={ProjectShowcase}></Route>
+                    <Route path="/projects/:name" exact component={ProjectShowcase}></Route>
                     <Route path="/members" exact component={Members}></Route>
                     <Route path="*" component={Home}></Route>
                     </Switch>
