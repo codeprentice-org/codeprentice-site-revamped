4813cc47ed2caa3b5d3526cc06848cc83512d1c2
Ansh Dhingra <56676059+anshdhinhgra47@users.noreply.github.com>
2020-12-06 01:39:45 +0530

    Initialized layout

--- frontend/src/components/members-page/members_page.tsx
+++ frontend/src/components/members-page/members_page.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../members-page/member/members_page.css';
 
 interface MembersProps {
 
@@ -6,7 +7,20 @@ interface MembersProps {
 
 const Members: React.FC<MembersProps> = () => {
     return (
-        <div style={{height: 'calc(100vh - 70px)', width: '100%'}}>
+        <div className="member_page_body">
+            <h1 className="main_heading">Members</h1>
+            <h2 className="founder_heading">Founders/Executive Board</h2>
+            <div className="founder_members">
+                <div className="member_body">
+
+                </div>
+                <div className="member_body">
+
+                </div>
+                <div className="member_body">
+
+                </div>
+            </div>
         </div>
     );
 };
