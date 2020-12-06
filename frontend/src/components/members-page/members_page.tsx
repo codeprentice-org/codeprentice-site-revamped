0cc197e4a54d70937a25ca9b1c8f942220e1314d
Sriram Sathish <63444773+sriram0107@users.noreply.github.com>
2020-12-06 14:00:09 +0400

    Merging files

--- frontend/src/components/members-page/members_page.tsx
+++ frontend/src/components/members-page/members_page.tsx
@@ -1,45 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-interface MembersProps {
 
-}
-
-const Member_name = "";
-const Member_desc = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ultrices vitae auctor eu augue ut lectus.";
-
-const Members: React.FC<MembersProps> = () => {
-    return (
-         <div className="projects_container">
-            <div className="projects_header">The Codeprentice Team</div>
-            <div className="project_display_container">
-                <Project name={"M1"} desc={Member_desc} />
-                <Project name={"M2"} desc={Member_desc}/>
-                <Project name={"M3"} desc={Member_desc} />
-                <Project name={"M4"} desc={Member_desc}/>
-                <Project name={"M5"} desc={Member_desc}/>
-                <Project name={"M6"} desc={Member_desc}/>
-                <Project name={"M7"} desc={Member_desc}/>
-                <Project name={Member_name} desc={Member_desc}/>
-                <Project name={Member_name} desc={Member_desc}/>       
-            </div>
-        </div>
-    );
-};
-
-interface MemberProps {
-    name: String,
-    desc: String 
-}
-
-const Project: React.FC<MemberProps> = (props) => {
-    return (
-        <Link to={``} className="project transform_background_color">
-            <div className="project_name">{props.name}</div>
-        </Link>
-            
-    )
-
-}
 
 export default Members;
