eda7044d718c05441590a315db29163293277639
Ansh Dhingra <56676059+anshdhinhgra47@users.noreply.github.com>
2020-12-06 14:52:37 +0530

    Completed founding members

--- frontend/src/components/members-page/members_page.tsx
+++ frontend/src/components/members-page/members_page.tsx
@@ -12,12 +12,44 @@ const Members: React.FC<MembersProps> = () => {
             <h2 className="founder_heading">Founders/Executive Board</h2>
             <div className="founder_members">
                 <div className="member_body">
+                    <div className="upper_part">
+
+                    </div>
+                    <div className="photo">
+                        <p className="member_photo">Photo</p>
+                    </div>
+                    <h2 className="name">NAME</h2>
+                    <div className="about">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse dolor similique consectetur, delectus quis veniam autem ipsam, iure quo illum corrupti possimus distinctio consequatur 
+                            officiis obcaecati sequi asperiores iste architecto?</p>
+                    </div>
+                </div>
+                <div className="member_body">
+                    <div className="upper_part">
+
+                    </div>
+                    <div className="photo">
+                        <p className="member_photo">Photo</p>
+                    </div>
+                    <h2 className="name">NAME</h2>
+                    <div className="about">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse dolor similique consectetur, delectus quis veniam autem ipsam, iure quo illum corrupti possimus distinctio consequatur 
+                            officiis obcaecati sequi asperiores iste architecto?</p>
+                    </div>
 
                 </div>
                 <div className="member_body">
+                    <div className="upper_part">
 
-                </div>
-                <div className="member_body">
+                    </div>
+                    <div className="photo">
+                        <p className="member_photo">Photo</p>
+                    </div>
+                    <h2 className="name">NAME</h2>
+                    <div className="about">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse dolor similique consectetur, delectus quis veniam autem ipsam, iure quo illum corrupti possimus distinctio consequatur 
+                            officiis obcaecati sequi asperiores iste architecto?</p>
+                    </div>
 
                 </div>
             </div>
